503022c85f05d7af8f559176581f51db504e3f82
root <root@beaglebone.(none)>
2014-04-23 22:24:20 +0000

    calling myo code

--- BBBK/onboot.py
+++ BBBK/onboot.py
@@ -2,7 +2,7 @@ import threading
 from subprocess import call
 from main import LampSaver
 from user_data_server import main as live_data_server
-from MYOlib.gesture import main as gesture_main
+#from MYOlib.gesture import main as gesture_main
 
 call(["gcc", "navigation.c", "-o", "nav", "-lm"])
 #call(["bash", "./init.sh"])
@@ -17,3 +17,9 @@ p1.start()
 
 #p2 = threading.Thread(target=live_data_server)
 #p2.start()
+
+try:
+    call(["python2", "./MYOlib/gesture.py"])
+except Exception as e:
+    print(e)
+
